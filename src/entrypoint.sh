8ba0e69b5bc2625aee6aa83dbfa16ab53d6a0f0f
Arpit Srivastava <arpt.svt@gmail.com>
2024-01-17 01:52:04 +0530

    Added nginx deployment

---
+++ src/entrypoint.sh
@@ -0,0 +1,7 @@
+APP_PORT=${PORT:-8000}
+cd /app/
+
+/opt/venv/bin/python manage.py migrate --noinput
+/opt/venv/bin/python manage.py collectstatic --noinput
+
+/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm eventchimp.wsgi:application --bind "0.0.0.0:${APP_PORT}"
